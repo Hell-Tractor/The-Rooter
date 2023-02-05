49c85b47d2f4354a1f3c920e00019d52b205f0b3
Hell-Tractor <1104840448@qq.com>
2023-02-05 16:04:31 +0800

    Finish start scene

--- Scripts/Controllers/AudioManager.cs
+++ Scripts/Controllers/AudioManager.cs
@@ -1,30 +1,30 @@
 using Godot;
 
-public class AudioManager : Node2D {
-    public static AudioManager Instance { get; private set; } = null;
-    [Export]
-    public NodePath BGMPlayerPath;
-    public AudioStreamPlayer2D BGMPlayer { get; private set; }
-    [Export]
-    public NodePath SEPlayerPath;
-    public AudioStreamPlayer2D SEPlayer { get; private set; }
+public class AudioManager : Node {
+	public static AudioManager Instance { get; private set; } = null;
+	[Export]
+	public NodePath BGMPlayerPath;
+	public AudioStreamPlayer2D BGMPlayer { get; private set; }
+	[Export]
+	public NodePath SEPlayerPath;
+	public AudioStreamPlayer2D SEPlayer { get; private set; }
 
-    public override void _EnterTree() {
-        if (Instance != null)
-            throw new System.Exception("Duplicate AudioManager exists.");
-        Instance = this;
+	public override void _EnterTree() {
+		if (Instance != null)
+			throw new System.Exception("Duplicate AudioManager exists.");
+		Instance = this;
 
-        this.BGMPlayer = this.GetNode<AudioStreamPlayer2D>(this.BGMPlayerPath);
-        this.SEPlayer = this.GetNode<AudioStreamPlayer2D>(this.SEPlayerPath);
-    }
+		this.BGMPlayer = this.GetNode<AudioStreamPlayer2D>(this.BGMPlayerPath);
+		this.SEPlayer = this.GetNode<AudioStreamPlayer2D>(this.SEPlayerPath);
+	}
 
-    public void PlayBGM(string path) {
-        this.BGMPlayer.Stream = GD.Load<AudioStream>(path);
-        this.BGMPlayer.Play();
-    }
+	public void PlayBGM(string path) {
+		this.BGMPlayer.Stream = GD.Load<AudioStream>(path);
+		this.BGMPlayer.Play();
+	}
 
-    public void PlaySE(string path) {
-        this.SEPlayer.Stream = GD.Load<AudioStream>(path);
-        this.SEPlayer.Play();
-    }
+	public void PlaySE(string path) {
+		this.SEPlayer.Stream = GD.Load<AudioStream>(path);
+		this.SEPlayer.Play();
+	}
 }
